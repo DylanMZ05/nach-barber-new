73e7d618e3e0bf84859899c24015212807d2af6c
DylanMZ <dylanmazurzgainer@gmail.com>
2025-01-23 14:24:21 -0300

    first commit

---
+++ nach-barbershop/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='w-screen h-auto bg-black'>
+        <div>
+          <img src="media/Logo.png" alt="" className='h-23 p-3'/>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
