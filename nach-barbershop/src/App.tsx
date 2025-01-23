5302055e977fa149aed725af651b54bada205709
DylanMZ <dylanmazurzgainer@gmail.com>
2025-01-23 16:28:33 -0300

    a

--- nach-barbershop/src/App.tsx
+++ nach-barbershop/src/App.tsx
@@ -8,9 +8,9 @@ function App() {
         <div className='flex justify-between items-center p-4 text-white'>
           <div className='flex items-center p-2 ml-10'>
             <img src="media/Logo.png" alt="" className='h-20 mr-2'/>
-            <p className='text-lg font-semibold'>NACH <br></br>BARBERSHOP</p>
+            <p className='text-lg font-semibold crimsontext tracking-wider ts-xl'>NACH <br></br>BARBERSHOP</p>
           </div>
-          <ul className='flex justify-between items-center w-175 mr-10 font-medium'>
+          <ul className='flex justify-between items-center w-175 mr-10 font-medium ts-xl'>
             <li>
               <a href="">Inicio</a>
             </li>
@@ -35,7 +35,27 @@ function App() {
 
       <main className='bg-[url("../media/background.jpg")] h-screen bg-cover bg-center'>
         
-        <section className='bg-black/40 bg-center h-screen '>
+        <section className='bg-black/40 bg-center h-screen w-screen flex flex-col justify-center items-center text-white'>
+
+          <img src="media/Logo.png" alt="" className='w-35 mb-[4vh] img-shadow'/>
+          <h1 className='text-5xl font-bold crimsontext mb-[2vh] ts-xl'>NACH BARBERSHOP</h1>
+          <h2 className='text-3xl font-bold mb-[1vh] text-shadow-xl ts-xl'>SANTA TERESITA</h2>
+
+          <div className='flex justify-between items-center'>
+
+            <div>
+              <p>Av 32 entre 3 y 4</p>
+              <p>Galería Aldea Paseo del Mar</p>
+            </div>
+
+            <hr className='h-13 w-0.5 bg-white rounded-xl opacity-70 ml-4 mr-4'/>
+
+            <div>
+              <p>Av 32 entre 3 y 4</p>
+              <p>Galería Aldea Paseo del Mar</p>
+            </div>
+
+          </div>
 
         </section>
 
