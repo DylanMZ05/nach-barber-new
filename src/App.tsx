dc39c3c4da9bcfcad28c1cc3e315a4e19578b2db
DylanMZ <dylanmazurzgainer@gmail.com>
2025-01-29 16:23:44 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,58 @@ import Prices from './components/prices';
 import Sucursales from './components/sucursales';
 import Footer from './components/footer';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
+import ScrollToTop from './hooks/ScrollToTop';
 import CortesDeCabello from './components/services-links/CortesDeCabello';
+import CortesBarba from './components/services-links/CortesBarba';
+import DiseñosCabello from './components/services-links/DiseñosCabello';
+import Productos from './components/services-links/Productos';
+
+const ScrollToHash: React.FC = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const element = document.querySelector(location.hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [location]);
+
+    return null;
+};
 
 function App() {
-  return (
-    <Router basename="/nach-barber-new">
-      <Header />
+    return (
+        <Router basename="/nach-barber-new">
+            <ScrollToTop />
+            <ScrollToHash />
+            <Header />
 
-      <Routes>
-        {/* Página principal */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Main />
-              <Services />
-              <Prices />
-              <Sucursales />
-            </>
-          }
-        />
-        {/* Rutas individuales */}
-        <Route path="/CortesDeCabello" element={<CortesDeCabello />} />
-        <Route path="/estilos-de-barba" element={<div>Estilos de Barba</div>} />
-        <Route path="/galeria-de-disenos" element={<div>Galería de Diseños</div>} />
-        <Route path="/productos" element={<div>Productos</div>} />
-      </Routes>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <Main />
+                            <Services currentPage="Inicio" />
+                            <Prices />
+                            <Sucursales />
+                        </>
+                    }
+                />
+                <Route path="/CortesDeCabello" element={<CortesDeCabello />} />
+                <Route path="/CortesBarba" element={<CortesBarba />} />
+                <Route path="/DiseñosCabello" element={<DiseñosCabello />} />
+                <Route path="/Productos" element={<Productos />} />
+            </Routes>
 
-      <Footer />
-    </Router>
-  );
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
